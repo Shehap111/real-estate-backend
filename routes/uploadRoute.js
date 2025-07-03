4e0008551e2780505adf60195281f03d5e408ae4
Shehap111 <shehap01097943039@gmail.com>
2025-07-03 18:45:50 +0300

    Update uploadRoute.js

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { upload } from '../config/uploadConfig.js';
-import uploadImage from '../controllers/uploadController.js';
+// import { upload } from '../config/uploadConfig.js';
+// import uploadImage from '../controllers/uploadController.js';
 import { verifyAdmin } from '../middleware/verifyAdmin.js';
 
 const router = express.Router();
 
-router.post('/upload', verifyAdmin, upload.single('image'), uploadImage);
-router.post('/upload-360', verifyAdmin, upload.single('image360'), uploadImage);
+// router.post('/upload', verifyAdmin, upload.single('image'), uploadImage);
+// router.post('/upload-360', verifyAdmin, upload.single('image360'), uploadImage);
 
 export default router;
