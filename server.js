c06646ef3fe491d9db2506333cf03285f90f65c9
Shehap111 <shehap01097943039@gmail.com>
2025-07-03 19:23:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import connectDB from './config/db.js';
+
 import cityRoutes from './routes/cityRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-import propertyTypeRoutes  from './routes/propertyTypeRoutes.js';
-import uploadRoute from './routes/uploadRoute.js';
-import path from 'path';
+import propertyTypeRoutes from './routes/propertyTypeRoutes.js';
 import propertyRoutes from './routes/propertyRoutes.js';
 import blogRoutes from './routes/blogRoutes.js';
-import contactRoutes from './routes/contactRoutes.js'
+import contactRoutes from './routes/contactRoutes.js';
+
 dotenv.config();
 const app = express();
 
-// Middlewares
+// Middleware
 app.use(cors({
-  origin: "http://localhost:3000", // أو دومين الموقع بتاعك لما ترفعه
+  origin: "http://localhost:3000", // غيره لدومين الفرونت لما ترفعه
   credentials: true,
 }));
-
 app.use(express.json());
 
 // Static folder for uploaded images
-app.use('/uploads', express.static(path.join('uploads')));
-
+// const __dirname = path.resolve();
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Routes
 app.use('/api/admin', adminRoutes);
 app.use('/api/cities', cityRoutes);
 app.use('/api/property-types', propertyTypeRoutes);
-app.use('/api', uploadRoute);
 app.use('/api/properties', propertyRoutes);
 app.use('/api/blog', blogRoutes);
 app.use('/api/contact', contactRoutes);
 
-// Connect to DB and Start Server
-// const PORT = process.env.PORT || 5000;
-// connectDB().then(() => {
-//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// });
+// Start server
+const PORT = process.env.PORT || 5000;
 
-export default app;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+});
