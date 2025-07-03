1bba4fae6e9a7521f8b7380aa86d5f1774e872fc
Shehap111 <shehap01097943039@gmail.com>
2025-07-03 19:42:00 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err.message);
+    console.log('✅ MongoDB Connected...');
+  } catch (error) {
+    console.error('❌ MongoDB Connection Failed:', error.message);
     process.exit(1);
   }
 };
